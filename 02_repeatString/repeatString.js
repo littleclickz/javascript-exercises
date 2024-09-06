87d672bddd4cb320e66b39ac710f46d83003d66a
littleclickz <hnvanpatten@gmail.com>
2024-09-05 18:47:16 -0700

    Solve exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    return string.repeat(times);
 };
+
+repeatString("hey", 3);
+
+repeatString("hello", 10);
+
+repeatString("hi", 1);
+
+repeatString("bye", 0);
+
+const number = Math.floor(Math.random() * 1000);
+repeatString("odin", number);
+
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
