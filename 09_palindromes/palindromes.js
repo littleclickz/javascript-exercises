28fcabec63af4b9b86587145630733a60279d835
littleclickz <hnvanpatten@gmail.com>
2024-09-07 10:10:31 -0700

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let bareString = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '');
+    let caseInsensitive = bareString.toLowerCase();
+    let reversedString = "";
+    for (let i = caseInsensitive.length - 1; i >= 0; i--) {
+        reversedString += caseInsensitive[i];
+    }
+    if (reversedString === caseInsensitive) {
+        return true;
+    }
+    else {
+        return false;
+    };
 };
 
 // Do not edit below this line
