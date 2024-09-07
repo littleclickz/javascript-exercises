9eb5395e62261dbc9b4ac30ffa4c5a81fd88ba66
littleclickz <hnvanpatten@gmail.com>
2024-09-06 20:55:01 -0700

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  let result = numOne + numTwo;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	let result = numOne - numTwo;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let arraySum = 0;
+  for (let i = 0; i <= (array.length - 1); i++) {
+    arraySum += array[i];
+  }
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let arraySum = 1;
+  for (let i = 0; i <= (array.length - 1); i++) {
+    arraySum *= array[i];
+  }
+  return arraySum;
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+	let result = numOne ** numTwo;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(numOne) {
+  let result = 1;
+  for (let i = numOne; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
