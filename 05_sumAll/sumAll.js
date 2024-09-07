7f4f52184fcacd621b2f5bb3154a7bc8cf1cca0e
littleclickz <hnvanpatten@gmail.com>
2024-09-06 18:57:22 -0700

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+      }
+      let sum = 0;
+      for (let i = min; i <= max; i++) {
+        sum += i;
+      }
+      return sum;
 };
+
+sumAll(1, 4);
+
+sumAll(1, 4000);
+
+sumAll(123, 1);
+
+sumAll(-10, 4);
+
+sumAll(2.5, 4);
+
+sumAll(10, "90");
+
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
